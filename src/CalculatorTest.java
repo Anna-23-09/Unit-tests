5a499d2d65b25f5553fc6dbcd3fb29e11327bfce
Anna <tolstovaaniuta@yandex.ru>
2023-02-16 13:12:24 +0300

    added test

--- src/CalculatorTest.java
+++ src/CalculatorTest.java
@@ -1,21 +1,18 @@
+import seminars.first.model.Calculator;
+import static org.assertj.core.api.Assertions.assertThat;
 public class CalculatorTest {
     public static void main(String[] args) {
-        testingDiscount(3000.00, 150);
+        assertThat(CalculatorTest.testingDiscount(3000.00, 15)).equals(2550.00);
+        assertThat(CalculatorTest.testingDiscount(3000.00, -15)).isEqualTo(false);
+
+        assert discountAmount > 0;
+        assert discountAmount < 100;
+        assert res < purchaseAmount;
     }
-
-//    public static double calculatingDiscount(double purchaseAmount, int discountAmount) {
-//
-//        double price = purchaseAmount - (purchaseAmount / 100 * discountAmount);
-//
-//        return price;
-//    }
-
     public static double testingDiscount (double purchaseAmount, int discountAmount) {
         double res = purchaseAmount - (purchaseAmount / 100 * discountAmount);
 
         return res;
-        assert discountAmount > 0;
-        assert discountAmount < 100;
-        assert res < purchaseAmount;
+
     }
 }
